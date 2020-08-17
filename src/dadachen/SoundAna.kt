6023e3c438cb07d8401c3ff2aa0faedd07a0f24e
DadaChen1997 <overlooker1997@gmail.com>
2020-08-17 17:10:51 +0800

    eliminate bugs

--- src/dadachen/SoundAna.kt
+++ src/dadachen/SoundAna.kt
@@ -3,25 +3,49 @@ package dadachen
 import java.io.File
 import javax.sound.sampled.AudioSystem
 
-
-fun main() {
-    val soundFile = "src/dadachen/fmcw-high.wav"
+fun printFreqDomain(path: String) {
+    val soundFile = path
     val audioInputStream = AudioSystem.getAudioInputStream(File(soundFile))
-    val indices = mutableListOf<Int>()
+    val indices = mutableListOf<Pair<Int, Int>>()
+    val lowIndexRange = 1620 until 1820
+    val highIndexRange = 1850 until 2049
     val listener = FrameListener { myStft, frAddr ->
         if (frAddr == -1L) {
-            System.err.println("ana finished")
             myStft.stop()
             var count = 0
+            var hcount = 0
+            var isone = true
+            var indexOne = 0
+            var indexSec = 0
+            var indexHigh = 0
+            var T = 882
             indices.forEachIndexed { index, i ->
-                if(index>0){
+                if (index > 0) {
                     count++
-                    if(indices[index-1]>i){
-                        println("the row num is $i, and the size is $count")
+                    hcount++
+                    if (count > 600 && indices[index - 1].first > i.first) {
+//                        println("low : from ${i.first} to ${indices[index - 1].first}, count: $count, time in index:${index}")
+                        if(isone){
+                            indexOne = index
+                        }else{
+                            indexSec = index
+                            val interval = ((indexSec-indexHigh+indexOne-indexHigh)+T)/2.0
+                            println("low: ($indexOne, $indexSec), high: $indexHigh, interval: $interval, time: ${1.0/4410*interval}")
+                        }
+                        isone = isone.toggle()
                         count = 0
                     }
-                }else{
+
+                    if (hcount > 600 && indices[index - 1].second > i.second) {
+//                        println("high: from ${i.second} to ${indices[index - 1].second}, time in index:${index}")
+                        indexHigh = index
+                        hcount = 0
+                    }
+
+                } else {
                     println("the total size is ${indices.size}")
+                    println("the row num is $i")
+
                     count++
                 }
             }
@@ -29,11 +53,28 @@ fun main() {
 
         } else {
             val frame = myStft.getFrame(frAddr)
-            val index = frame.indexOf(frame.max() ?: 0.0)
-            indices.add(index)
+            val highFrame = frame.filterIndexed { index, _ ->
+                index in highIndexRange
+            }
+            val lowFrame = frame.filterIndexed { index, _ ->
+                index in lowIndexRange
+            }
+            val lowIndex = lowFrame.indexOf(lowFrame.max() ?: 0.0) + 1620
+            val highIndex = highFrame.indexOf(highFrame.max() ?: 0.0) + 1850
+            val element = Pair(lowIndex, highIndex)
+            indices.add(element)
         }
     }
-    val stft = STFT(audioInputStream, 4096, 10, 1024)
+    val stft = STFT(audioInputStream, 4096, 10, 900)
     stft.addFrameListener(listener)
     stft.start()
+}
+
+fun Boolean.toggle():Boolean = !this
+
+fun main() {
+    val isLow = false
+    val path = "src/dadachen/fmcw_${if (isLow) "low" else "high"}.wav"
+    val testPath = "src/dadachen/test.wav"
+    printFreqDomain(testPath)
 }
--- src/dadachen/SoundAnaFreq.kt
+++ src/dadachen/SoundAnaFreq.kt
@@ -5,19 +5,33 @@ import javax.sound.sampled.AudioSystem
 
 
 fun main() {
-    val soundFile = "src/dadachen/fmcw-high.wav"
+    val soundFile = "src/dadachen/fmcw_low.wav"
     val audioInputStream = AudioSystem.getAudioInputStream(File(soundFile))
+    val index = mutableListOf<Int>()
     val listener = FrameListener { myStft, frAddr ->
         if (frAddr == -1L) {
             System.err.println("ana finished")
             myStft.stop()
-
+            var count = 0
+            index.forEachIndexed { id, i ->
+                count++
+                if(id>0){
+                    if(index[id-1]>i){
+                        println("$i size:$count and pre: ${index[id-1]}")
+                        count=0
+                    }
+                }else{
+                    print(i)
+                    count=0
+                }
+            }
+            println("size: ${index.size}")
         } else {
             val frame = myStft.getFrame(frAddr)
-
+            index.add(frame.indexOf(frame.max()?:0.0))
         }
     }
-    val stft = STFT(audioInputStream, 4096, 10, 1024)
+    val stft = STFT(audioInputStream, 4096, 10, 1)
     stft.addFrameListener(listener)
     stft.start()
 }
